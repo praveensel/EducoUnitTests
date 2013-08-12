822f7fc1a77e26a3a3b6a71ea3948a4cc11f83b9
praveensel <micbonem4g>
2013-08-12 16:05:39 +0530

    test

--- src/test/java/com/educo/tests/Helpers/Credentials.java
+++
@@ -1,9 +0,0 @@
-package com.educo.tests.Helpers;
-
-public class Credentials {
-
-    public static String LOGIN_USERNAME = "educotestmsgs@hotmail.com";
-    public static String LOGIN_PASSWORD = "welcome";
-    public static String USA_URL = "https://www.educosoft.com/Home/US/es_u_default.aspx";  // TO BE REFACTORED!!
-    public static String India_URL = "https://www.educosoft.com/Home/IN/es_i_default.aspx";  // TO BE REFACTORED!!
-}
